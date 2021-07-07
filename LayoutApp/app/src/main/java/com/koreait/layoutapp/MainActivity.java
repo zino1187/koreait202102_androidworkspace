825e4e46e00a34d14f24a0a5c5eb030fee0cfe36
zino1187 <zino1187@gmail.com>
2021-07-07 11:02:48 +0900

    c

--- LayoutApp/app/src/main/java/com/koreait/layoutapp/MainActivity.java
+++ LayoutApp/app/src/main/java/com/koreait/layoutapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.q7); //화면에 보여질 뷰를 XML을 이용하는 방법
+        setContentView(R.layout.q10); //화면에 보여질 뷰를 XML을 이용하는 방법
 
         //XML이 아닌 순수 자바코드로 디자인을 구현해보자(비효율적이지만, 때에 따라서는 필요하다)
         /*
