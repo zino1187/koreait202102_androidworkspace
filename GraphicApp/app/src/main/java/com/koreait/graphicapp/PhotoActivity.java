44e3a134e1cc3cb83dd965318872847361e71818
zino1187 <zino1187@gmail.com>
2021-07-16 13:51:01 +0900

    c

--- GraphicApp/app/src/main/java/com/koreait/graphicapp/PhotoActivity.java
+++ GraphicApp/app/src/main/java/com/koreait/graphicapp/PhotoActivity.java
@@ -3,12 +3,45 @@ package com.koreait.graphicapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class PhotoActivity extends AppCompatActivity {
+public class PhotoActivity extends AppCompatActivity implements View.OnClickListener {
+    Button bt_prev, bt_auto, bt_next;
+    PhotoView photoView;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gallery);
+        setContentView(R.layout.activity_gallery); //인플레이션 종료
+        
+        bt_prev=findViewById(R.id.bt_prev);
+        bt_auto=findViewById(R.id.bt_auto);
+        bt_next=findViewById(R.id.bt_next);
+        photoView=findViewById(R.id.photoView);
+
+        //이벤스소스와 리스너연결
+        bt_prev.setOnClickListener(this);
+        bt_auto.setOnClickListener(this);
+        bt_next.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.equals(bt_prev)){
+
+        }else if(v.equals(bt_auto)){
+
+        }else if(v.equals(bt_next)){
+
+        }
     }
 }
+
+
+
+
+
+
+
+
+
--- GraphicApp/app/src/main/java/com/koreait/graphicapp/PhotoView.java
+++ GraphicApp/app/src/main/java/com/koreait/graphicapp/PhotoView.java
@@ -1,19 +1,38 @@
 package com.koreait.graphicapp;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
-import androidx.annotation.Nullable;
-
 public class PhotoView extends View {
+
+    Bitmap[] bitmaps=new Bitmap[10]; //10개를 준비하겠다
+    int index=0; //몇번째 인덱스를 보고싶은지를 결정짓는 변수
+
     public PhotoView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        bitmaps[0]=BitmapFactory.decodeResource(context.getResources(), R.drawable.iron);
+        bitmaps[1]=BitmapFactory.decodeResource(context.getResources(), R.drawable.babarian);
+        bitmaps[2]=BitmapFactory.decodeResource(context.getResources(), R.drawable.batman);
+        bitmaps[3]=BitmapFactory.decodeResource(context.getResources(), R.drawable.beam);
+        bitmaps[4]=BitmapFactory.decodeResource(context.getResources(), R.drawable.deadpool);
+        bitmaps[5]=BitmapFactory.decodeResource(context.getResources(), R.drawable.hulk);
+        bitmaps[6]=BitmapFactory.decodeResource(context.getResources(), R.drawable.magni);
+        bitmaps[7]=BitmapFactory.decodeResource(context.getResources(), R.drawable.spider);
+        bitmaps[8]=BitmapFactory.decodeResource(context.getResources(), R.drawable.wolf);
+        bitmaps[9]=BitmapFactory.decodeResource(context.getResources(), R.drawable.woman);
+
+        //이미지 크기 조정
+        for(int i=0;i<bitmaps.length;i++){
+            bitmaps[i]=bitmaps[i].createScaledBitmap(bitmaps[i], 600,550, true);
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+        canvas.drawBitmap(bitmaps[index], 50, 50, null);
     }
 }
