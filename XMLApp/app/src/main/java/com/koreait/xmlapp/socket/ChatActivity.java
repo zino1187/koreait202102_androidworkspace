5cff13471295b399149720e4bce5cd42d0f52a2e
zino1187 <zino1187@gmail.com>
2021-07-20 10:40:42 +0900

    c

---
+++ XMLApp/app/src/main/java/com/koreait/xmlapp/socket/ChatActivity.java
@@ -0,0 +1,17 @@
+package com.koreait.xmlapp.socket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.koreait.xmlapp.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+    }
+}
