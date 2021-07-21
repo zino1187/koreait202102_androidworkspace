0cd6342eafae7483a1531e410fb76db29a762e7d
zino1187 <zino1187@gmail.com>
2021-07-21 13:20:33 +0900

    c

--- FragmentApp/app/src/main/java/com/koreait/fragmentapp/MainActivity.java
+++ FragmentApp/app/src/main/java/com/koreait/fragmentapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.koreait.fragmentapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -10,25 +11,38 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.koreait.fragmentapp.fragments.BlueFragment;
 import com.koreait.fragmentapp.fragments.RedFragment;
+import com.koreait.fragmentapp.fragments.YellowFragment;
 
 public class MainActivity extends AppCompatActivity {
-    RedFragment redFragment;
+    FragmentManager manager;
+    Fragment[] fragments=new Fragment[3]; //비어있는 프레그먼트형 배열 준비
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //프레그먼트 생성 및 뷰안에 넣기!!
-        redFragment = new RedFragment();
+        fragments[0]= new RedFragment();
+        fragments[1] = new YellowFragment();
+        fragments[2] = new BlueFragment();
 
         //기본적으로 액티비티는 프레그먼트를 사용할수 있기 때문에 메니저를 얻어오는 능력이 있다
-        FragmentManager manager=this.getSupportFragmentManager();
+        manager=this.getSupportFragmentManager();
 
+        //최초에 보여질 페이지
+        FragmentTransaction transaction =manager.beginTransaction();
+        transaction.add(R.id.fragment_container, fragments[0]);
+        transaction.commit(); //확정짓기
+    }
+
+    //어떤 프레그먼트를 보여줄지를 결정짓는 메서드
+    public void showPage(int index){
+        //프레그먼트 생성 및 뷰안에 넣기!!
         FragmentTransaction transaction =manager.beginTransaction();
         //화면에 프레그먼트를 등장시킬지 여부를확정지을 수 있는 객체
-        transaction.add(R.id.fragment_container, redFragment);
+        transaction.replace(R.id.fragment_container, fragments[index]);
         transaction.commit(); //확정짓기
     }
 
@@ -42,16 +56,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //아이템 이벤트처리
-
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
-            case R.id.item_red:
-                ;break;
-            case R.id.item_yellow:
-                ;break;
-            case R.id.item_blue:
-                ;break;
+            case R.id.item_red:showPage(0);break;
+            case R.id.item_yellow:showPage(1);break;
+            case R.id.item_blue:showPage(2);break;
         }
         return super.onOptionsItemSelected(item);
     }
