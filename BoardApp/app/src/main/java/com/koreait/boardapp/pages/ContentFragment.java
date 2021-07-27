55dd97cdc14a7bd92c9dd7c0b02f69156cafc558
zino1187 <zino1187@gmail.com>
2021-07-27 10:51:41 +0900

    c

--- BoardApp/app/src/main/java/com/koreait/boardapp/pages/ContentFragment.java
+++ BoardApp/app/src/main/java/com/koreait/boardapp/pages/ContentFragment.java
@@ -1,6 +1,7 @@
 package com.koreait.boardapp.pages;
 
 import android.os.Bundle;
+import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,12 +13,27 @@ import androidx.fragment.app.Fragment;
 
 import com.koreait.boardapp.MainActivity;
 import com.koreait.boardapp.R;
+import com.koreait.boardapp.domain.Board;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ContentFragment extends Fragment {
     String TAG=this.getClass().getName();
     MainActivity mainActivity;
     EditText t_title, t_writer, t_content;
     Button bt_list, bt_update, bt_delete;
+    String ip="172.30.1.53";
+    int port=8888;
 
     @Override
     //반환값이 View는, 현재의 프레그먼트에서 보여줄 뷰를 의미
@@ -47,6 +63,63 @@ public class ContentFragment extends Fragment {
             Log.d(TAG, "람다로 작동성공");
         });
         return view;
+    }
+
+    public void update(){
+        URL url=null;
+        HttpURLConnection con=null;
+        BufferedWriter buffw=null;
+
+        try {
+            url=new URL("http://"+ip+":"+port+"/rest/board");
+            con=(HttpURLConnection) url.openConnection();
+            con.setRequestMethod("PUT");//요청 방법 지정(post, get ... )
+            con.setRequestProperty("Content-Type","application/json;charset=utf-8");
+            con.setDoOutput(true); //post방식은 데이터를 전송함이 목적...
+
+            //string형으로 서버에 json문자열을 전송하자!!
+            StringBuilder sb = new StringBuilder();
+            sb.append("{");
+            sb.append("\"board_id\":"+mainActivity.board.getBoard_id()+",");
+            sb.append("\"title\":\""+t_title.getText().toString()+"\",");
+            sb.append("\"writer\":\""+t_writer.getText()+"\",");
+            sb.append("\"content\":\""+t_content.getText()+"\"");
+            sb.append("}");
+
+            //con객체로부터 스트림을 뽑은후, 데이터 출력
+            buffw = new BufferedWriter(new OutputStreamWriter(con.getOutputStream(), "UTF-8"));
+            buffw.write(sb.toString()+"\n");
+            buffw.flush();
+
+            int code=con.getResponseCode(); //서버의 응답코드 200 등등
+            Log.d("WriteFragment","수정 후 결과 코드"+code);
+
+            String msg="";
+            if(code==200){
+                msg="수정성공";
+            }else{
+                msg="수정실패";
+            }
+            Message message = new Message();
+            Bundle bundle = new Bundle();
+            bundle.putString("msg",msg);
+            message.setData(bundle);
+            //handler.sendMessage(message);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            if(buffw!=null){
+                try {
+                    buffw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 
     @Override
